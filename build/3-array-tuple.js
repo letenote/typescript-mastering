34d667ecffe1a237b76b187c11886dd9c0b760a6
aditReza <lete.note3@gmail.com>
2022-01-27 22:26:35 +0700

    add types array and tuple

--- build/3-array-tuple.js
+++ build/3-array-tuple.js
@@ -1,2 +1,48 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const inferenceArrayNumber = [1, 2, 3, 4];
+let bracketArrayNumber;
+bracketArrayNumber = [1, 2, 3, 4];
+let genericArrayNumber;
+genericArrayNumber = [1, 2, 3, 4];
+const inferenceArrayString = ['a', 'b', 'c'];
+let bracketArrayString;
+bracketArrayString = ['a', 'b', 'c'];
+let genericArrayString;
+genericArrayString = ['a', 'b', 'c'];
+const inferenceArrayUnion = ["hello", 1, 2, 3];
+let bracketArrayUnion;
+bracketArrayUnion = ["hello", 1, 2, 3];
+let genericArrayUnion;
+genericArrayUnion = ["hello", 1, 2, 3];
+let bracketTupple;
+bracketTupple = ['cordinat', 2, 4, 7];
+const arrayOfObjectInference = [
+    { color: 'blue', index: 1 },
+    { color: 'red', index: 2 },
+];
+let arrayOfObjectBracket;
+arrayOfObjectBracket = [
+    { color: 'blue', index: 1 },
+    { color: 'red', index: 2 },
+];
+let arrayOfObjectGeneric;
+arrayOfObjectGeneric = [
+    { color: 'blue', index: 1 },
+    { color: 'red', index: 2 },
+];
+const arrayMultiDimension = [
+    [1, 2],
+    [3, 4],
+];
+let arrayMultiDimensionBracket;
+arrayMultiDimensionBracket = [
+    [1, 2],
+    [3, 4],
+];
+let arrayMultiDimensionGeneric;
+arrayMultiDimensionGeneric = [
+    [1, 2],
+    [3, 4],
+];
+let [a, b, c, d] = [1, 2, 3, 'four'];
--- src/3-array-tuple.ts
+++ src/3-array-tuple.ts
@@ -17,20 +17,55 @@ export {}; // quick fix for global variable
  * [1,2,3,4]
  */
 
+// type inference
+const inferenceArrayNumber = [1,2,3,4]
+// type bracket
+let bracketArrayNumber: number[]
+    bracketArrayNumber = [1,2,3,4]
+// type generic array
+let genericArrayNumber: Array<number>
+    genericArrayNumber = [1,2,3,4]
+
 /**
  * 2. Array of String
  * ['a', 'b', 'c']
  */
+
+// type inference
+const inferenceArrayString = ['a', 'b', 'c']
+// type bracket
+let bracketArrayString: string[]
+    bracketArrayString = ['a', 'b', 'c']
+// type generic array
+let genericArrayString: Array<string>
+    genericArrayString = ['a', 'b', 'c']
 
 /**
  * 3. Array with Union
  * ["hello", 1, 2, 3 ]
  */
 
+// type inference
+const inferenceArrayUnion = ["hello", 1, 2, 3 ]
+// type bracket
+let bracketArrayUnion: (string | number)[]
+    bracketArrayUnion = ["hello", 1, 2, 3 ]
+// type generic array
+let genericArrayUnion: Array<string | number>
+    genericArrayUnion = ["hello", 1, 2, 3 ]
+
 /**
  * 4. Tuple
  * ['cordinat', 2, 4, 7]
  */
+
+// type inference
+// dont use type inference for tuple
+// type bracket
+let bracketTupple: [string, number, number, number]
+    bracketTupple = ['cordinat', 2, 4, 7]
+// type generic tuple
+// dont use type generic for tuple
 
 /**
  * 5. Array of Object
@@ -40,6 +75,24 @@ export {}; // quick fix for global variable
  * ]
  */
 
+// type inference
+const arrayOfObjectInference =  [
+     { color: 'blue', index: 1 },
+     { color: 'red', index: 2 },
+   ]
+// type bracket
+let arrayOfObjectBracket: { color: string, index: number }[]
+    arrayOfObjectBracket = [
+      { color: 'blue', index: 1 },
+      { color: 'red', index: 2 },
+    ]
+// type generic array
+let arrayOfObjectGeneric: Array<{ color: string, index: number }>
+    arrayOfObjectGeneric = [
+      { color: 'blue', index: 1 },
+      { color: 'red', index: 2 },
+    ]
+
 /**
  * 6. Array multi dimension [[]]
  * matrix = [
@@ -48,7 +101,27 @@ export {}; // quick fix for global variable
  * ];
  */
 
+// type inference
+const arrayMultiDimension = [
+    [1, 2],
+    [3, 4],
+   ];
+// type bracket
+let arrayMultiDimensionBracket: number[][]
+    arrayMultiDimensionBracket = [
+      [1, 2],
+      [3, 4],
+    ];
+// type generic array
+let arrayMultiDimensionGeneric: Array<Array<number>>
+    arrayMultiDimensionGeneric = [
+      [1, 2],
+      [3, 4],
+    ];
+
 /**
  * 7. Destructuring
- * let [a, b, c, d]: [number, number, number, string] = [1, 2, 3, 'four'];
+ * let [a, b, c, d] = [1, 2, 3, 'four'];
  */
+
+let [a, b, c, d]: [number, number, number, string] = [1, 2, 3, 'four'];
